a69619e3a2c9ea291e936480a61d309bcbe78d05
denisg28 <53035874+denisg28@users.noreply.github.com>
2019-08-12 14:14:44 +0300

    Worked application with logger_12.08

--- trello-web/src/test/java/com/telran/tests/fw/ApplicationManager.java
+++ trello-web/src/test/java/com/telran/tests/fw/ApplicationManager.java
@@ -1,10 +1,15 @@
 package com.telran.tests.fw;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 
 import java.lang.reflect.Method;
@@ -19,6 +24,26 @@ public class ApplicationManager {
 
  WebDriver driver;
     public String browser;
+
+    public static class MyListener extends AbstractWebDriverEventListener
+    {
+        Logger logger = LoggerFactory.getLogger(MyListener.class);
+
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+        logger.info("Start search element " +by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            logger.info("element" + by + "found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            logger.error(throwable.toString());
+        }
+    }
 
     public ApplicationManager(String browser)
     {
@@ -90,7 +115,4 @@ public class ApplicationManager {
       return profile;
   }
 
-
-
-  
 }
